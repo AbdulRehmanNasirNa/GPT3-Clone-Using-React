f28704e3f60ca69a7a302aeab838e3a93af6e888
AbdulRehmanNasirNa <85030864+AbdulRehmanNasirNa@users.noreply.github.com>
2023-01-12 02:38:06 -0800

    Add files via upload
    initial commit

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+import "./article.css"
+const Article = ({imgUrl ,date,title}) => {
+  return (
+    <div className='gpt3__blog-container__article'>
+    <div className='gpt3__blog-container__article-image'>
+      <img src={imgUrl} alt="Blog" />
+    </div>
+    <div className='gpt3__blog-container__article-content'>
+      <div> <p>{date}</p>
+      <h3>{title}</h3>
+      <p>Read Full Article</p>
+      </div>
+  
+    </div>
+    </div>
+  )
+}
+
+export default Article
---
+++ src/components/cta/CTA.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+import "./cta.css"
+const CTA = () => {
+  return (
+    <div className='gpt3__cta'>
+      <div className='gpt3__cta-content'>
+      <h3>Request Early Access to Get Started</h3>
+        <p>
+        Register today & start exploring the endless possiblities.
+        </p>
+       
+      </div>
+      <div className='gpt3__cta-button'> <button type='button'> Get started</button></div>
+    </div>
+  )
+}
+
+export default CTA
